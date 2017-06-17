3406378c375842ad837e38a886a8199afab50bdc
briwoo <briwoo1999@gmail.com>
2017-06-17 01:59:53 -0500

    table of contents

---
+++ ch6/tableOfContents.rb
@@ -0,0 +1,5 @@
+width = 46
+puts('Table of Contents'.center(width))
+puts('Chapter 1: Getting Started'.ljust(20) + 'page  1'.rjust(20))
+puts('Chapter 2: Numbers'.ljust(26) + 'page  9'.rjust(20))
+puts('Chapter 3: Letters'.ljust(26) + 'page 13'.rjust(20))
