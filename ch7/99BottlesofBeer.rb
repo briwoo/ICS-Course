d26e91f96fe7eee4c15e714fbda1fa0efd148467
briwoo <briwoo1999@gmail.com>
2017-06-18 14:34:13 -0500

    99 bottles of beer

---
+++ ch7/99BottlesofBeer.rb
@@ -0,0 +1,18 @@
+bottles = 99
+while bottles>1
+  puts bottles.to_s + ' bottles of beer on the wall'
+  puts bottles.to_s + ' bottles of beer'
+  puts 'take one down and pass it around'
+  bottles = bottles-1
+  puts bottles.to_s + ' bottles of beer on the wall'
+  puts ''
+end
+puts '1 bottle of beer on the wall'
+puts '1 bottle of beer'
+puts 'take one down and pass it around'
+puts 'no more bottles of beer on the wall'
+puts ''
+puts 'no more bottles of beer on the wall'
+puts 'no more bottles of beer'
+puts 'Go to the store and buy some more'
+puts '99 bottles of beer on the wall'
